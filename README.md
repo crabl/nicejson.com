71799c9c8d3b4a6af6df7fc9d02363a20bd3de20
Chris Rabl <rabl.christopher@gmail.com>
2018-06-08 00:00:47 -0600

    feat: add mostly complete app

---
+++ README.md
@@ -0,0 +1,25 @@
+# NiceJSON.com
+## a web-based, privacy-focused JSON pretty printer
+
+### Development
+
+```
+$ git clone git@github.com:crabl/nicejson.com.git
+$ cd nicejson.com
+$ npm install
+$ npm run dev
+```
+
+To build the production artifacts:
+
+```
+npm run build
+```
+
+### To Do List
+
+- [x] more forgiving json parser
+- [x] privacy policy
+- [x] bundle with parcel
+- [x] open-source it on github
+  
---
+++ index.js
@@ -0,0 +1,42 @@
+const { parse, AST } = require('json-ast');
+
+const $paste = document.getElementById('paste');
+const $result = document.getElementById('json');
+const $instructions = document.getElementById('instructions');
+const $error = document.getElementById('error');
+
+const ERROR_SYMBOL = Symbol();
+
+function formatJSON(json_string) {
+  try {
+    const ast = parse(json_string, { junker: true });
+    const parsed = AST.JsonNode.toJSON(ast);
+    return JSON.stringify(parsed, null, 2);
+  } catch (e) {
+    return ERROR_SYMBOL;
+  }
+}
+
+$paste.onpaste = (event) => {
+  let raw_paste;
+
+  if (window.clipboardData && window.clipboardData.getData) {
+    raw_paste = window.clipboardData.getData('Text');
+  } else if (event.clipboardData && event.clipboardData.getData) {
+    raw_paste = event.clipboardData.getData('text/plain');
+  }
+
+  const result = formatJSON(raw_paste);
+
+  if (result === ERROR_SYMBOL) {
+    $result.style.display = 'none';
+    $instructions.style.display = 'none';
+    $error.style.display = 'block';
+  } else {
+    $result.innerHTML = result;
+    $instructions.style.display = 'none';
+    $error.style.display = 'none';
+  }
+
+  return false; // prevent default
+};
